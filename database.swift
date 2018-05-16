1bd1767abac3bf8934e36817a3bd25b7dd64c6b6
wangvivian <954457232@qq.com>
2018-05-16 18:30:18 +1000

    Add files via upload

---
+++ database.swift
@@ -0,0 +1,59 @@
+import Foundation
+//
+//  File.swift
+//  SIT206_Assignment2
+//
+//  Created by 王子璇 on 2018/5/10.
+//  Copyright © 2018年 wangyuwei. All rights reserved.
+//
+import UIKit
+import CoreLocation
+import MapKit
+import CoreData
+
+////////////class
+class Registers {
+    static var registers = [UserInfo]()
+    static var viewContext : NSManagedObjectContext?
+    
+    static func getViewContext() -> NSManagedObjectContext {
+        if viewContext == nil {
+            viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        }
+        return viewContext!
+    }
+    static func getRegisters() -> [UserInfo] {
+        if registers.count == 0 { loadRegisters() }
+        return registers
+    }
+    
+    static func addRegister(username : String, password : String, usersdob : Date, email : String, tel: String) {
+        let context = getViewContext()
+        let register = UserInfo(context: context)
+        register.username = username
+        register.password = password
+        register.usersdob = usersdob
+        register.email = email
+        register.tel = tel
+        try! context.save()
+    }
+    
+    static func getRegister(at : Int) -> UserInfo? {
+        if registers.count < 1 { loadRegisters() }
+        
+        if at >= 0 && at < registers.count {
+            return registers[at]
+        }
+        return nil
+    }
+    static func loadRegisters() {
+        let context = getViewContext()
+        registers = try! context.fetch(UserInfo.fetchRequest())
+        
+       
+        
+    }
+    
+    
+    
+}
