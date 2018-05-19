6074fbb4dfc27ae03b0727de8d3a3f6d17532963
wangvivian <954457232@qq.com>
2018-05-19 16:49:10 +1000

    Add files via upload

--- UserSettingViewController.swift
+++ UserSettingViewController.swift
@@ -1,35 +1,83 @@
-//
-//  UserSettingViewController.swift
-//  SIT206_Assignment2
-//
-//  Created by 王子璇 on 2018/5/10.
-//  Copyright © 2018年 wangyuwei. All rights reserved.
-//
+
 
 import UIKit
 
 class UserSettingViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBOutlet weak var singerimg: UIImageView!
+    @IBOutlet weak var nowplayinglabel: UILabel!
+    @IBOutlet weak var songprogress: UIProgressView!
+    @IBOutlet weak var elapsedtimelabel: UILabel!
+    @IBOutlet weak var remainingtimelabel: UILabel!
+    @IBOutlet weak var volumslider: UISlider!
+    var filterbysinger = "All"
+    var musicplayer : MusicPlayer = MusicPlayer (PlayList())
+    var timer : Timer!
+    var playorresume : Bool = false
+    var isplaying : Bool = false
+    
+    @IBAction func play(_ sender: UIButton) {
+        if playorresume == true {
+            musicplayer.resume()
+        }
+        else {
+            musicplayer.play(filterBy: filterbysinger)
+        }
+        updateUI()
+        timer = Timer.scheduledTimer(timeInterval: 1,target : self, selector : #selector(self.updaterealtimeUI),
+                                     userInfo: nil, repeats: true)
     }
-    */
-
+   
+    @IBAction func previous(_ sender: UIButton) {
+        musicplayer.previous(filterBy: filterbysinger)
+        updateUI()
+    }
+    @IBAction func stop(_ sender: UIButton) {
+        musicplayer.stop()
+        playorresume = false
+        updateUI()
+    }
+    @IBAction func next(_ sender: UIButton) {
+        musicplayer.next(filterBy: filterbysinger)
+        updateUI()
+    }
+    @IBAction func changevolume(_ sender: UISlider) {
+        musicplayer.changeVolume(newValue: sender.value)
+    }
+    
+    @objc func updaterealtimeUI(){
+        let currentsong = musicplayer.getCurrentSong()
+        let progress = Float (currentsong.timeElapsed / currentsong.duration)
+        songprogress.setProgress(progress, animated: true)
+        let elapsedtime = Int(currentsong.timeElapsed.nextUp)
+        let remainingtime = Int(currentsong.duration.nextUp) - elapsedtime
+        elapsedtimelabel.text = "\(elapsedtime)"
+        remainingtimelabel.text = "\(remainingtime)"
+    }
+    func updateUI(){
+        let currentsong = musicplayer.currentSong
+        singerimg.image = UIImage(named : currentsong.singer)
+        nowplayinglabel.text = currentsong.title
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
---
+++ animationViewController.swift
@@ -0,0 +1,62 @@
+//
+//  animationViewController.swift
+//  SIT206_Assignment2
+//
+//  Created by 王子璇 on 2018/5/19.
+//  Copyright © 2018年 wangyuwei. All rights reserved.
+//
+
+import UIKit
+
+class animationViewController: UIViewController {var gravity : UIGravityBehavior?
+    var animator : UIDynamicAnimator?
+    var gametimer : Timer?
+    
+    
+    
+
+override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    gametimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:#selector(animationViewController.addBalloon(_:)), userInfo: nil, repeats: true)
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity = UIGravityBehavior(items: [])
+        
+        let vector = CGVector(dx: 0.0, dy: 0.1)
+        gravity?.gravityDirection = vector
+        animator?.addBehavior(gravity!)
+    }
+    
+    @objc func addBalloon(_ : Any){
+        let xcoordinate = arc4random() % UInt32(self.view.bounds.width)
+        
+        let btn = UIButton(frame: CGRect(x: Int(xcoordinate), y: 60, width: 50, height: 50))
+        btn.setImage(UIImage(named: "option2"), for: .normal)
+        btn.addTarget(self, action: #selector(self.didimage(_:)), for: .touchUpInside)
+        self.view.addSubview(btn)
+        gravity?.addItem((btn as UIView))
+    }
+    
+    @IBAction func didimage(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "option2"), for: .normal)
+        UIView.animate(withDuration: 0.4, animations: {sender.alpha = 0}, completion: {(true) in sender.removeFromSuperview()})
+        
+    }
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
